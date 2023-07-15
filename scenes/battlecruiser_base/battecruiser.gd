2d0c53a926adfd020f86cfab96f52305bd8958d4
Nico Viarnes <nico@viarn.es>
2023-07-15 12:13:34 -0700

    edit world

--- scenes/battlecruiser_base/battecruiser.gd
+++ scenes/battlecruiser_base/battecruiser.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	apply_central_impulse(transform.y * -2)
+	apply_central_impulse(transform.y * - 1)
